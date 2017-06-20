bdea84cabf10cc0c3d6e29aefa8a79a0fe4e729f
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-20 19:17:42 -0400

    Images now stop and start and ratings are displayed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,17 @@ var apiKey = "e4abc46fc4cc480fa6bc9fd01ad3061b";
 var numberDisplayed = 10;
 var play = false;
 
+// These arrays will store image links
+var stillImages = [];
+var animatedImages = [];
+
+function turnAnimated(imageIndex){
+	$('#display' + imageIndex).attr("src",animatedImages[imageIndex]);
+}
+
+function turnStill(imageIndex){
+	$('#display' + imageIndex).attr("src",stillImages[imageIndex]);
+}
 
 function getStills(searchTerm){
 
@@ -21,9 +32,25 @@ function getStills(searchTerm){
 	    }).done(function(info) {
 	      for (var x = 0; x < numberDisplayed; x++){
 	      	var imageLink = info.data[x].images.fixed_height_still.url;
-	      	$('#image-holder').append("<img class='gif-image' id='display" + x + "' src='" + imageLink + "'>");
-
+	      	stillImages.push(imageLink);
+	      	var animLink = info.data[x].images.fixed_height.url;
+	      	animatedImages.push(animLink);
+	      	var imageRating = info.data[x].rating;
+	      	$('#image-holder').append("<img class='gif-image' id='display" + x + "' src='" + imageLink + "'><p class='rating'>" + imageRating + "</p>");
 	      }
+
+	      $(".gif-image").click(function() {
+			var localID = this.id;
+			var localIndex = localID.split("y")[1];
+			if (play){
+				turnStill(localIndex)
+				play = false;
+			} else {
+				turnAnimated(localIndex);
+				play = true;
+			}
+			
+		  });
 	    });
 };
 
@@ -32,6 +59,10 @@ $( document ).ready(function() {
 	$( ".topic-buttons" ).click(function() {
 		var buttonTerm = this.id;
 		$('#image-holder').empty();
+		stillImages = [];
+		animatedImages = [];
 		getStills(buttonTerm);
 	});
+
+
 });
