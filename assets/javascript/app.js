4a41209fcabe3ad1167b9c3c07cb8621c496a84c
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-20 18:49:19 -0400

    Buttons now work correctly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,14 @@
 var topics = ["Jay Z", "Beyonnce", "Kanye West", "2 Chainz", "J Cole", "Rhianna", "Drake", "Ed Sheeran", "Taylor Swift", "Justin Beiber", "Justin Timberlak", "Darth Vader"];
 
 function generateButtons(){
-	console.log("Function running");
 	for (var i = 0; i < topics.length; i++){
-		console.log(topics[i]);
 		$('#button-holder').append("<button class='topic-buttons' id='" + topics[i] + "'>" + topics[i] + "</button>");
 	}
 }
 
 var apiKey = "e4abc46fc4cc480fa6bc9fd01ad3061b";
 var numberDisplayed = 10;
+var play = false;
 
 
 function getStills(searchTerm){
@@ -20,15 +19,19 @@ function getStills(searchTerm){
 	      url: queryURL,
 	      method: "GET"
 	    }).done(function(info) {
-	      console.log(info);
-	      for (var x = 0; x <= numberDisplayed; x++){
+	      for (var x = 0; x < numberDisplayed; x++){
 	      	var imageLink = info.data[x].images.fixed_height_still.url;
-	      	$('#image-holder').append("<img src='" + imageLink + "'>");
+	      	$('#image-holder').append("<img class='gif-image' id='display" + x + "' src='" + imageLink + "'>");
 
 	      }
 	    });
 };
 
-$( ".topic-buttons" ).click(function() {
-  
+
+$( document ).ready(function() {
+	$( ".topic-buttons" ).click(function() {
+		var buttonTerm = this.id;
+		$('#image-holder').empty();
+		getStills(buttonTerm);
+	});
 });
